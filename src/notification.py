79881a0085d970edd31f248161330e976b4aaedb
Latesil <skipastas@gmail.com>
2020-02-06 00:34:50 +0300

    add desktop notifications

--- src/notification.py
+++
@@ -1,9 +0,0 @@
-import gi
-gi.require_version('Notify', '0.7')
-from gi.repository import Notify
-
-Notify.init('com.github.Latesil.folder-cleaner')
-
-notification = Notify.Notification.new('Title', "body")
-
-notification.show()
